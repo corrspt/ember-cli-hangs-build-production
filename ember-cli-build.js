7a6f266064110a0edab6ca4177e04583309a559a
Pedro Rio <pedro.rio@gmail.com>
2015-11-09 15:34:00 +0000

    Make ember-cli hang

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -6,6 +6,8 @@ module.exports = function(defaults) {
     // Add options here
   });
 
+  app.import('vendor/kendo.all.min.js');
+
   // Use `app.import` to add additional libraries to the generated
   // output files.
   //
